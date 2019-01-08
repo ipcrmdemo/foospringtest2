fd3b28abae2bbecb435dd6367ca6a4854ce1d679
Matt Cadorette <mcadorette@gmail.com>
2019-01-08 12:32:54 -0500

    Initial commit from Atomist

---
+++ src/main/java/com/foospringtest2/Foospringtest2Application.java
@@ -0,0 +1,12 @@
+package com.foospringtest2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Foospringtest2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Foospringtest2Application.class, args);
+	}
+}
